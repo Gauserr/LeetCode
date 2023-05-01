908db5c258b2886237b33f6177e0ccd88d9cf6c9
Ryan Gause <gauserr@icloud.com>
2023-05-01 02:14:55 -0400

    Time: 162 ms (91.10%) | Memory: 51.6 MB (26.70%) - LeetSync

--- palindrome-number/palindrome-number.js
+++ palindrome-number/palindrome-number.js
@@ -3,11 +3,5 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  const array = String(x).split('')
-  console.log(array)
-  if (array.length === 1 || array.length === 0)
-    return true
-  if (array[0] !== array[array.length - 1])
-    return false
-  return isPalindrome(array.slice(1, -1).join(''))
+  return x === +x.toString().split('').reverse().join('')
 };
