5fb2174c0bcdc7268c77dd09499f17a6c3ac98fa
Ryan Gause <ryan.g.gause.3@gmail.com>
2023-07-13 03:21:21 -0400

    Time: 56 ms (94.98%) | Memory: 42.7 MB (44.86%) - LeetSync

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -7,7 +7,7 @@ var twoSum = function(nums, target) {
    const store = {};
    for (let i = 0; i < nums.length; i++){
      const comp = target - nums[i];
-     if (String(comp) in store) return [store[comp], i]
+     if (comp in store) return [store[comp], i]
      store[nums[i]] = i;
    }   
 };
