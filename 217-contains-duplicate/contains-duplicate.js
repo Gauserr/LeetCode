3f305c15cae63a09da255af56b3e93808ed369fd
Ryan Gause <ryan.g.gause.3@gmail.com>
2023-08-02 00:36:29 -0400

    Time: 73 ms (98.18%) | Memory: 55.1 MB (33.44%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,10 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-   const store = new Set()
-   for (let num of nums){
-     if (store.has(num)) return true
-     store.add(num)
-   }
-   return false
+   return new Set(nums).size !== nums.length
+   
 };
