d101ace5443664fb3c3de428c15925df57b7f92b
Ryan Gause <ryan.g.gause.3@gmail.com>
2023-08-03 01:54:10 -0400

    Time: 67 ms (91.98%) | Memory: 49.4 MB (23.50%) - LeetSync

---
+++ 271-encode-and-decode-strings/encode-and-decode-strings.js
@@ -0,0 +1,43 @@
+/**
+ * Encodes a list of strings to a single string.
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var encode = function(strs) {
+  let s = ''
+  for (let string of strs){
+    s += (string.length) + '#' + string
+  }
+  console.log(s)
+  return s
+};
+
+/**
+ * Decodes a single string to a list of strings.
+ *
+ * @param {string} s
+ * @return {string[]}
+ */
+var decode = function(s) {
+  const strs = []
+
+  let i = 0 
+  while (i < s.length){
+    let j = i
+    while (s[j] !== '#'){
+      j = j + 1
+    }
+    const len = parseInt(s.slice(i, j))
+    const str = s.slice(j+1, j + len + 1)
+    strs.push(str)
+    i = j + len + 1
+  }
+  
+  return strs
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
