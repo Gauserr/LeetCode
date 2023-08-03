9e2e2b13f3bb7de7f4d0c6e90175ee29f1552f27
Ryan Gause <ryan.g.gause.3@gmail.com>
2023-08-03 03:21:59 -0400

    Time: 53 ms (93.18%) | Memory: 43.1 MB (19.51%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+   let l = 0
+   let r = numbers.length - 1
+   while (l < r){
+     const sum = numbers[l] + numbers[r]
+     if (sum === target) return [l+1, r+1]
+     if (sum < target) l++
+     if (sum > target) r--
+   }
+   return false
+};
