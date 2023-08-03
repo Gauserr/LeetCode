338b303fdff060eab40b70c574567843490c7692
Ryan Gause <ryan.g.gause.3@gmail.com>
2023-08-03 00:10:09 -0400

    Time: 515 ms (13.99%) | Memory: 54.3 MB (56.00%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+   const prefix = []
+   for (let i = 0; i < nums.length; i++){
+     if (i === 0) prefix.push(nums[0])
+     else prefix.push(nums[i] * prefix[i - 1])
+   }
+
+  let postfix = []
+  for (let i = nums.length - 1; i >= 0; i--){
+    if (i === nums.length - 1) postfix.unshift(nums[i])
+    else postfix.unshift(nums[i] * postfix[0])
+  }
+
+  const result = []
+  for (let i = 0; i < nums.length; i++){
+    if (i === 0) result.push(postfix[i+1])
+    else if (i === nums.length - 1) result.push(prefix[i - 1])
+    else result.push(prefix[i-1] * postfix[i+1])
+  }
+
+  return result
+}
