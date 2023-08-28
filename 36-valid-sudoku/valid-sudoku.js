ff8d3f3df056948ed70e4317f72d0fbeabbb056b
Ryan Gause <ryan.g.gause.3@gmail.com>
2023-08-28 04:07:30 -0400

    Time: 67 ms (72.47%) | Memory: 45.1 MB (59.26%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -11,24 +11,21 @@ var isValidSudoku = function(board) {
         for (let j = 0; j < board[0].length; j++){
             const value = board[i][j]
             if (value === '.') continue
+
             if (!rows[j]) rows[j] = new Set()
             const squareCoordinates = `${parseInt(i/3)},${parseInt(j/3)}`
             if (!squares[squareCoordinates]) squares[squareCoordinates] = new Set()
-            if (squares[squareCoordinates].has(value)) {
-                console.log('false coords')
+
+            if (squares[squareCoordinates].has(value))
                 return false
-            }
             squares[squareCoordinates].add(value)
-            if (columns[i].has(value)){
-                console.log('false column')
-                console.log(columns[i])
+
+            if (columns[i].has(value))
                 return false
-            }
             columns[i].add(value)
-            if (rows[j].has(value)) {
-                console.log('false row')
+            
+            if (rows[j].has(value))
                 return false
-            }
             rows[j].add(value)
         }
     }
