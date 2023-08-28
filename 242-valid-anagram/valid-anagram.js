c507a9ba60e6458304cf4e68731b915c48a9e89d
Ryan Gause <ryan.g.gause.3@gmail.com>
2023-08-28 03:25:31 -0400

    Time: 76 ms (62.88%) | Memory: 44.6 MB (51.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -11,7 +11,6 @@ var isAnagram = function(s, t) {
     sStore[letter] = sStore[letter] ? sStore[letter] + 1 : 1
   for (let letter of t)
     tStore[letter] = tStore[letter] ? tStore[letter] + 1 : 1
-  console.log(sStore, tStore)
   for (let letter in sStore){
     if (!(letter in tStore))
       return false
